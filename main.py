3831db012515d70dd85158742899556392a6cd52
Rageeni Dawale <rageenidawale@gmail.com>
2022-05-11 22:40:05 +0530

    Add code to play morse code and save it to a .wav file

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import sounddevice as sd
+import soundfile as sf
+import numpy as np
+from scipy.io.wavfile import write
+
 # Create a dictionary that contains the letters and their morse code as the key value pair.
 MORSE_CODE_DICT = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
                    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
@@ -31,3 +36,34 @@ for char in lst:
 morse_code_sentence = " ".join(morse_code_list)
 
 print(morse_code_sentence)
+
+# Listen and record the morse code
+dot = 'sounds/dot.wav'
+dash = 'sounds/dash.wav'
+space = 'sounds/space.wav'
+# Create and array to create a combined audio
+sound_array = []
+# Create a pause to distinguish between each character
+pause = np.zeros([20000, 2], dtype="float32")
+
+for code in morse_code_sentence:
+    if code == ".":
+        data, fs = sf.read(dot)
+    elif code == "-":
+        data, fs = sf.read(dash)
+    else:
+        data, fs = sf.read(space)
+
+    # Append the sounds whether dot/dash/space to the sound array
+    sound_array.append(sd.playrec(data,
+                                  fs,
+                                  channels=2,
+                                  dtype="float32",
+                                  blocking=True))
+    # Append a pause after each character
+    sound_array.append(pause)
+
+# Create a numpy array
+new = np.vstack(sound_array)
+# Write this numpy array to the output.wav file
+write(f'output.wav', 44100, new)
